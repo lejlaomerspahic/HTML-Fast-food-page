8a396d2eb2a65e85b1f9b13ec53a63ab8977a039
lejlaomerspahic <96488468+lejlaomerspahic@users.noreply.github.com>
2022-05-14 18:46:13 +0200

    PUT i DELETE metoda

--- script.js
+++ script.js
@@ -6,38 +6,76 @@ fetch('https://ptf-web-dizajn-2022.azurewebsites.net/api/Food')
   return res.json();
 })
 .then((data) => {
-      let contentWrapper = document.querySelector(".content-wrapper");
-      data.forEach((food) => {
-        let card = document.createElement("div");
-        card.classList.add("card");
+  foods=data;
+  renderFoods(data);
+  })
 
+  renderFoods = (foods) => {
+    let containerList = document.querySelector('.container');
+    let resultHtml = '';
 
-        let image = document.createElement("img");
-        image.src += food.imageUrl;
-        card.appendChild(image);
-
-        let cardWrapper = document.createElement("div");
-        cardWrapper.classList.add("card-body");
-
-        let userFullName = document.createElement("p");
-        userFullName.innerText = "Product ID : " + food.id;
-
-        let userName = document.createElement("p");
-        userName.innerText = "Name of the product: " + food.name;
-
-        let userCompany = document.createElement("p");
-        userCompany.innerText = "Product price: " + food.price;
-        cardWrapper.appendChild(userFullName);
-        cardWrapper.appendChild(userName);
-        cardWrapper.appendChild(userCompany);
-
-        card.appendChild(cardWrapper);
-        contentWrapper.appendChild(card);
-      });
-    })
-    .catch((error) => {
-      alert(error);
+    foods.forEach(food => {
+        resultHtml += `
+        
+        <div class="card">
+            <img src="${food.imageUrl}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h1 class="card-title">${food.name}</h1>
+            <p>Product price: ${food.price} KM</p>
+            <button type="button" onclick="fillData(${food.id})" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Uredi proizvod</button>
+            <button type="button" class="btn btn-danger" onclick="deleteFOOD(${food.id})">Obrisi proizvod</button>
+            
+            </div>
+        </div>
+        `;
     });
+    containerList.innerHTML = resultHtml;
+  }
 
+
+
+  const fillData = (foodID) => {
+    const food=foods.find(food => food.id === foodID)
+    const FoodFormID=document.getElementById("food-id");
+    const FoodFormName=document.getElementById("food-name");
+    const FoodFormPrice=document.getElementById("food-price");
+    const FoodFormURL=document.getElementById("food-url");
+    
+    FoodFormID.value=food.id;
+    FoodFormName.value=food.name;
+    FoodFormPrice.value=food.price;
+    FoodFormURL.value=food.imageUrl;
+    }
+
+  const putFood=()=>{
+  const FoodFormID=document.getElementById("food-id").value;
+  const FoodFormName=document.getElementById("food-name").value;
+  const FoodFormPrice=document.getElementById("food-price").value;
+  const FoodFormURL=document.getElementById("food-url").value;
+
+  
+  fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food`, {
+        method: 'PUT',
+        headers: new Headers({'content-type': 'application/json'}),
+        body: JSON.stringify({
+          id: FoodFormID,
+          name: FoodFormName,
+          price: FoodFormPrice,
+          imageUrl: FoodFormURL
+        })
+    })
+    .then(res => {
+        console.log(res);
+    })
+  }
+
+  const deleteFOOD = (id) => {
+    fetch(`https://ptf-web-dizajn-2022.azurewebsites.net/api/Food/${id}`, {
+        method: 'DELETE'
+    })
+    .then(res => {
+        console.log(res);
+    })
+}
 
 
